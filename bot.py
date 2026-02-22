c00913d83bbd26a374cb291239fb553cec1f6259
abrek1010155-blip <abrek1010155@gmail.com>
2026-02-23 00:05:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram import Update 
 import asyncio
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-from telegram.error import TimedOut, NetworkError
-TOKEN = "твой_токен_сюда"  # замени на свой
+import os
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Как тебя зовут?")
@@ -13,11 +12,11 @@ async def handle_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
     name = update.message.text.strip()
     await update.message.reply_text(f"Круто, {name}! Теперь я тебя знаю. Что дальше?")
 
-def main():
+async def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_name))
-    app.run_polling()
+    await app.run_polling()
 
 if __name__ == '__main__':
     loop = asyncio.new_event_loop()
